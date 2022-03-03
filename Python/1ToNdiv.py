830e35b251e0154d031b6c1866201042a81658cb
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-03 16:19:28 +0530

    Update 1ToNdiv.py

--- Python/1ToNdiv.py
+++ Python/1ToNdiv.py
@@ -3,7 +3,7 @@ divisor = int(input("enter divisior"))
 
 for i in range(1, number):
     if i % divisor == 0:
-        print(i, "Is Prime :", number % 2 == 0)
+        print(i, "Is Even :", number % 2 == 0)
 
     else:
         pass
