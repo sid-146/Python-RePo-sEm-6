daf23074a411d38013329baf9eb2b8fb0c42fc54
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2022-04-01 16:08:21 +0530

    Create DTypes.py

---
+++ Python/DTypes.py
@@ -0,0 +1,18 @@
+a = int(10)
+print(a, type(a))
+
+b = 1.0
+# b = float(1.0)
+print(b,type(b))
+
+c = 1+2j
+print(c,type(c))
+
+l = [1,2,3,4,5]
+print(l ,type(l))
+
+t = (1,2,3,4,5)
+print(t,type(t))
+
+s = 'Sudhanwa Kaveeshwar'
+print(s,type(s))
